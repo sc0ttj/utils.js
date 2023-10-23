396532abfaee661afffa758ab4e59627df92684c
Scott J <sc0ttj@users.noreply.github.com>
2023-10-23 11:29:41 +0100

    Update dom.js

--- dom.js
+++ dom.js
@@ -4,6 +4,8 @@
 //
 //
 
+const isFirefox = !!window.InternalError;
+
 const clearCookies = document.cookie.split(';').forEach(cookie => document.cookie = cookie
         .replace(/^ +/, '')
         .replace(/=.*/, `=;expires=${new Date(0).toUTCString()};path=/`)
