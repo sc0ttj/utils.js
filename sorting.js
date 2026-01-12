4a983fb02e039f92dc6248381145be8cf001b265
Scott J <sc0ttj@users.noreply.github.com>
2026-01-12 15:39:16 +0000

    Update sorting.js
    - fixed date comparison logic (which returned booleans instead of the required 1, -1, or 0) 
    - fixed the typeof check for dates (which should use instanceof Date)
    - fixed `sortOrder`, needed a default return value.

--- sorting.js
+++ sorting.js
@@ -6,22 +6,22 @@ const numbersAsc = (a, b) => a - b;
 const numbersDesc = (a, b) => b - a;
 const stringsAsc = (a, b) => a > b ? 1 : -1;
 const stringsDesc = (a, b) => b > a ? 1 : -1;
-const datesAsc = (a, b) => a.getTime() < b.getTime();  // least recent at top
-const datesDesc = (a, b) => a.getTime() > b.getTime(); // most recent at top 
+const datesAsc = (a, b) => a.getTime() - b.getTime();  // least recent at top
+const datesDesc = (a, b) => b.getTime() - a.getTime(); // most recent at top 
 
 const randomised = () => Math.random() - 0.5;
 const randomized = randomised;
 
-const ascending = (a,b) => {
-  if (typeof a === 'string' && typeof b === 'string') return stringsAsc(a,b);
-  else if (typeof a === 'date' && typeof b === 'date') return datesAsc(a,b);
-  else return numbersAsc(a,b);
+const ascending = (a, b) => {
+  if (typeof a === 'string' && typeof b === 'string') return stringsAsc(a, b);
+  else if (a instanceof Date && b instanceof Date) return datesAsc(a, b);
+  else return numbersAsc(a, b);
 };
 
-const descending = (a,b) => {
-  if (typeof a === 'string' && typeof b === 'string') return stringsDesc(a,b);
-  else if (typeof a === 'date' && typeof b === 'date') return datesDesc(a,b);
-  else return numbersDesc(a,b);
+const descending = (a, b) => {
+  if (typeof a === 'string' && typeof b === 'string') return stringsDesc(a, b);
+  else if (a instanceof Date && b instanceof Date) return datesDesc(a, b);
+  else return numbersDesc(a, b);
 };
 
 // Sort strings according to the rules of the users current system locale.
@@ -40,7 +40,7 @@ const stringsIntl = (a,b, opts = {}) => a.localeCompare(b, navigator.languages[0
 //  const mostRecent = sortBy('date', 'desc');
 //  const sortedData = array.sort(mostRecent);
 const sortBy = (prop, order) => 
-  (a, b) => (order === 'desc') ? descending(a[prop],b[prop]) : ascending(a[prop],b[prop]);
+  (a, b) => (order === 'desc') ? descending(a[prop], b[prop]) : ascending(a[prop], b[prop]);
 
 // Returns a sort function that applies multiple sort functions, in the given order.
 //
@@ -53,7 +53,7 @@ const sortOrder = (...sortFuncs) =>
   (a, b) => {
     for (const sort of sortFuncs) {
       let res = sort(a, b);
-      if (res !== 0) return res; // items not equal (in sorting terms) so no more criteria to apply
+      if (res !== 0) return res;  // items not equal (in sorting terms) so no more criteria to apply
     }
-    return res;
+    return 0; 
   };
